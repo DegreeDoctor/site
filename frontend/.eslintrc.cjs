67f25f60c42df2ad91837696487f0fec9878e2a3
Ryzon3 <ryanmcole20@gmail.com>
2022-09-08 15:32:49 -0400

    Created Vue project

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "plugin:vue/vue3-strongly-recommended",
+        "plugin:vue/vue3-recommended",
+        "eslint:recommended",
+        "@vue/eslint-config-prettier",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    rules: {
+        "no-unused-vars": "warn",
+        "vue/no-unused-vars": "warn",
+
+        "vue/html-indent": ["warn", 4],
+        indent: ["warn", 4],
+
+        "vue/max-attributes-per-line": [
+            "warn",
+            {
+                singleline: 4,
+                multiline: 4,
+            },
+        ],
+    },
+};
