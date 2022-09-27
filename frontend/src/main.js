dc019fc59c7f8895ada4968e4401684fedfffc47
Ryzon3 <ryanmcole20@gmail.com>
2022-09-27 16:33:03 -0400

    Added Quasar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,23 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { Quasar } from 'quasar'
 
 import App from "./App.vue";
 import router from "./router";
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
-
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
 app.use(pinia);
 app.use(router);
 
