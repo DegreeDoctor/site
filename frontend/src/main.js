9135dfcf00f4acb6231839405bbe6a4c465da6dd
Ryzon3 <ryanmcole20@gmail.com>
2022-09-30 12:32:56 -0400

    General fixes for Site

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { Quasar } from 'quasar'
+import { Quasar } from "quasar";
 
 import App from "./App.vue";
 import router from "./router";
 
 // Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
+import "@quasar/extras/material-icons/material-icons.css";
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import "quasar/src/css/index.sass";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+    plugins: {}, // import Quasar plugins and add here
 });
 app.use(pinia);
 app.use(router);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: { transformAssetUrls },
+        }),
         quasar({
-            sassVariables: 'src/quasar-variables.sass'
-        })
+            sassVariables: "src/quasar-variables.sass",
+        }),
     ],
     resolve: {
         alias: {
