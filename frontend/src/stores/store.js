9a099b56217fcd93d6feb8c2d173d079c9cecad6
Ryzon3 <ryanmcole20@gmail.com>
2022-10-11 15:50:02 -0400

    Fixed how credits are stored, Closes #24

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -4,7 +4,8 @@ import { useStorage } from "@vueuse/core";
 export const useStore = defineStore("main", {
     state: () => ({
         degrees: useStorage("degrees", []),
-        credits: useStorage("credits", 0),
+        // Object of lists formatted as: {"course1": 4, "Course2", 3}
+        credits: useStorage("credits", {}),
     }),
     //Act like computed in Vue
     getters: {
@@ -20,8 +21,23 @@ export const useStore = defineStore("main", {
         addDegree() {
             this.degrees.push("Degree");
         },
-        changeCredits(amount) {
-            this.credits += amount;
+        addCredits(name, amount) {
+            if(!this.credits[name]) {
+                this.credits[name] = amount;
+            }
         },
+        removeCredits(name) {
+            if(this.credits[name]) {
+                delete this.credits[name];
+            }
+        },
+        changeCredits(name, amount) {
+            if(this.credits[name]) {
+                this.credits[name] = amount;
+            }
+        },
+        fetchCredits(name) {
+            return this.credits[name];
+        }
     },
 });
