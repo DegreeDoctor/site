68a6bb08c42ea7602a22e1c706a68a0a12621824
Kenneth Dong <dongkenneth256@gmail.com>
2022-10-21 16:10:27 -0400

    Working Form Field

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -11,6 +11,8 @@ export default {
 </script>
 
 <template>
-    <button @click="store.addDegree()">test button</button>
-    <router-link to="degree">test</router-link>
+    <q-form action="https://some-url.com" method="post">
+    <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="createValue"
+            :options="filterOptions" @filter="filterFn" style="width: 250px" />
+    </q-form>
 </template>
