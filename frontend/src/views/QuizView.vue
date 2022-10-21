932fde8c7d194c246f00275d84a7a6bf38812d58
Kenneth Dong <dongkenneth256@gmail.com>
2022-10-21 16:16:31 -0400

    Working Form Input to Select Major

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -1,5 +1,12 @@
 <script>
 import { useStore } from "../stores/store";
+import { ref } from 'vue';
+
+
+
+const stringOptions = [
+    'Computer Science', 'Information Technology Web Science', 'Math', 'Physics', 'Chemistry', 'Biology', 'History', 'Geography', 'Economics', 'Psychology', 'Sociology', 'Philosophy', 'English', 'French', 'German', 'Spanish', 'Italian', 'Russian', 'Chinese', 'Japanese', 'Arabic', 'Other'
+]
 
 export default {
     data() {
@@ -7,12 +14,46 @@ export default {
             store: useStore(),
         };
     },
-};
+    setup() {
+        const filterOptions = ref(stringOptions)
+
+        return {
+            model: ref(null),
+            filterOptions,
+
+            createValue(val, done) {
+
+                if (val.length > 0) {
+                    if (!stringOptions.includes(val)) {
+                        stringOptions.push(val)
+                    }
+                    done(val, 'toggle')
+                }
+            },
+
+            filterFn(val, update) {
+                update(() => {
+                    if (val === '') {
+                        filterOptions.value = stringOptions
+                    }
+                    else {
+                        const needle = val.toLowerCase()
+                        filterOptions.value = stringOptions.filter(
+                            v => v.toLowerCase().indexOf(needle) > -1
+                        )
+                    }
+                })
+            }
+        }
+    }
+}
 </script>
+
+
 
 <template>
     <q-form action="https://some-url.com" method="post">
-    <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="createValue"
+        <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="createValue"
             :options="filterOptions" @filter="filterFn" style="width: 250px" />
     </q-form>
 </template>
