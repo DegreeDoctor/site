052d5f50f3d3620a4c67e80935bad008a5f63a11
Kenneth Dong <dongkenneth256@gmail.com>
2022-10-21 16:57:19 -0400

    Committing buggy code.

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -1,50 +1,70 @@
 <script>
 import { useStore } from "../stores/store";
-import { ref } from 'vue';
-
-
-
-const stringOptions = [
-    'Computer Science', 'Information Technology Web Science', 'Math', 'Physics', 'Chemistry', 'Biology', 'History', 'Geography', 'Economics', 'Psychology', 'Sociology', 'Philosophy', 'English', 'French', 'German', 'Spanish', 'Italian', 'Russian', 'Chinese', 'Japanese', 'Arabic', 'Other'
-]
 
 export default {
     data() {
         return {
             store: useStore(),
+            selectedMajors: [],
+            majorSearch: "",
+            minorSearch: "",
+            selectedMinors: [],
+            majorOptions: ['Computer Science', 'Information Technology Web Science', 'Math',
+                'Physics', 'Chemistry', 'Biology', 'History', 'Geography', 'Economics', 'Psychology',
+                'Sociology', 'Philosophy', 'English', 'French', 'German', 'Spanish', 'Italian',
+                'Russian', 'Chinese', 'Japanese', 'Arabic', 'Other'],
+            minorOptions: ['Monkey Science', 'Information Technology Web Science',
+                'Math', 'Physics', 'Chemistry', 'Biology', 'History', 'Geography',
+                'Economics', 'Psychology', 'Sociology', 'Philosophy', 'English',
+                'French', 'German', 'Spanish', 'Italian', 'Russian', 'Chinese',
+                'Japanese', 'Arabic', 'Other'],
         };
     },
-    setup() {
-        const filterOptions = ref(stringOptions)
+    computed: {
 
-        return {
-            model: ref(null),
-            filterOptions,
+        filterMajor() {
+            if (this.majorSearch === '') {
+                return this.majorOptions;
+            }
+            else {
+                const needle = this.majorSearch.toLowerCase()
+                return this.majorOptions.filter(
+                    v => v.toLowerCase().indexOf(needle) > -1
+                )
+            }
+        },
 
-            createValue(val, done) {
-
-                if (val.length > 0) {
-                    if (!stringOptions.includes(val)) {
-                        stringOptions.push(val)
-                    }
-                    done(val, 'toggle')
-                }
-            },
-
-            filterFn(val, update) {
-                update(() => {
-                    if (val === '') {
-                        filterOptions.value = stringOptions
-                    }
-                    else {
-                        const needle = val.toLowerCase()
-                        filterOptions.value = stringOptions.filter(
-                            v => v.toLowerCase().indexOf(needle) > -1
-                        )
-                    }
-                })
+        filterMinor() {
+            if (this.minorSearch === '') {
+                return this.minorOptions;
+            }
+            else {
+                const needle = this.minorSearch.toLowerCase()
+                return this.minorOptions.filter(
+                    v => v.toLowerCase().indexOf(needle) > -1
+                );
             }
         }
+    },
+    methods: {
+        addMajor(val, done) {
+            if (val.length > 0) {
+                if (!this.selectedMajors.includes(val)) {
+                    this.selectedMajors.push(val)
+                }
+                done(val, 'toggle')
+            }
+        },
+
+        addMinor(val, done) {
+
+            if (val.length > 0) {
+                if (!this.selectedMinors.includes(val)) {
+                    this.selectedMinors.push(val)
+                }
+                done(val, 'toggle')
+            }
+        },
     }
 }
 </script>
@@ -52,8 +72,13 @@ export default {
 
 
 <template>
-    <q-form action="https://some-url.com" method="post">
-        <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="createValue"
-            :options="filterOptions" @filter="filterFn" style="width: 250px" />
+    <q-form action="https://some-url.com" method="post" class="fixed-center">
+        <p>Select Your Major(s):</p>
+        <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="addMajor"
+            :options="filterMajor" style="width: 250px" />
+        <p>Select Your Minor(s):</p>
+
+        <q-select filled v-model="model" use-input use-chips multiple input-debounce="0" @new-value="addMinor"
+            :options="filterMinor" style="width: 250px" />
     </q-form>
 </template>
