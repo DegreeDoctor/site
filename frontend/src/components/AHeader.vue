229bbe5bc700f24f24a490594794e5ce93eb9267
liny18 <liny18@rpi.edu>
2022-10-24 19:53:09 -0400

    updated header
    fixed the pop up to working and put in temporary static data as placeholders

--- frontend/src/components/AHeader.vue
+++ frontend/src/components/AHeader.vue
@@ -2,7 +2,13 @@
 import { useStore } from "../stores/store";
 
 export default {
-
+    data() {
+        return {
+            prompt: false,
+            current: "CS Plan",
+            plans: ["CS Plan", "ITWS Plan", "HASS Plan"]
+        };
+    }
 }
 </script>
 
@@ -18,19 +24,38 @@ export default {
 
     <div class="q-pa-md q-gutter-sm">
         <q-btn label="Prompt" color="primary" @click="prompt = true"></q-btn>
-        <q-dialog v-model="prompt" persistent>
+        <q-dialog v-model="prompt">
             <q-card style="min-width: 350px">
             <q-card-section>
-                <div class="text-h6">Your address</div>
+                <div class="text-h6">Pick a plan</div>
             </q-card-section>
 
             <q-card-section class="q-pt-none">
-                <q-input dense v-model="address" autofocus @keyup.enter="prompt = false"></q-input>
+                <!-- <q-input dense v-model="address" autofocus @keyup.enter="prompt = false"></q-input> -->
+                <h6 class="q-ma-none">Current Plan: {{ current }}</h6>
+                <q-select
+                    filled
+                    v-model="current"
+                    use-input
+                    input-debounce="0"
+                    label="Simple filter"
+                    :options="plans"
+                    style="width: 200px"
+                    behavior="menu"
+                >
+                    <template v-slot:no-option>
+                    <q-item>
+                        <q-item-section class="text-grey">
+                        No results
+                        </q-item-section>
+                    </q-item>
+                    </template>
+                </q-select>
             </q-card-section>
 
             <q-card-actions class="text-primary">
                 <q-btn flat label="Cancel" v-close-popup></q-btn>
-                <q-btn flat label="Add address" v-close-popup></q-btn>
+                <q-btn flat label="Add new plan" v-close-popup></q-btn>
             </q-card-actions>
             </q-card>
         </q-dialog>
