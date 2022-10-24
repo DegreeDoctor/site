f2863e27a5b3aa14a5a85e9f7cff0e2e6dd3feff
liny18 <liny18@rpi.edu>
2022-10-24 11:11:13 -0400

    added header
    added a header component with logo and button to a pop up

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import { RouterView } from "vue-router";
+import AHeader from "./components/AHeader.vue";
 
 export default {
     components: {
         RouterView,
+        AHeader,
     }
 }
 </script>
 
 <template>
     <q-layout view="hHh LpR fFf">
+        <AHeader />
         <q-page-container>
             <RouterView />
         </q-page-container>
---
+++ frontend/src/components/AHeader.vue
@@ -0,0 +1,45 @@
+<script>
+import { useStore } from "../stores/store";
+
+export default {
+
+}
+</script>
+
+<template>
+    <q-header elevated class="bg-secondary text-white" height-hint="98">
+        <q-toolbar>
+        <q-toolbar-title>
+            <q-avatar>
+                <img src="../assets/Degree_Doctor_logo.png">
+            </q-avatar>
+            Degree Doctor
+        </q-toolbar-title>
+
+    <div class="q-pa-md q-gutter-sm">
+        <q-btn label="Prompt" color="primary" @click="prompt = true"></q-btn>
+        <q-dialog v-model="prompt" persistent>
+            <q-card style="min-width: 350px">
+            <q-card-section>
+                <div class="text-h6">Your address</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+                <q-input dense v-model="address" autofocus @keyup.enter="prompt = false"></q-input>
+            </q-card-section>
+
+            <q-card-actions class="text-primary">
+                <q-btn flat label="Cancel" v-close-popup></q-btn>
+                <q-btn flat label="Add address" v-close-popup></q-btn>
+            </q-card-actions>
+            </q-card>
+        </q-dialog>
+    </div>
+        </q-toolbar>
+    </q-header>
+</template>
+<!-- 
+Icon, left most
+light mode switch, right
+popup button, right of switch
+-->
