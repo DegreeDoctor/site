c3c4359c18a4013d734d37e20d4f7d9dfd1fff45
Ryzon3 <ryanmcole20@gmail.com>
2022-10-25 16:15:18 -0400

    Course Holder file made

---
+++ frontend/src/components/CourseHolder.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    props: {
+        inCourse: {
+            type: Object,
+            required: false,
+            default: null
+        }
+    }
+    data() {
+        return {
+            course: this.inCourse
+        }
+    }
+}
+</script>
