afe940517c874bafc2b347a89e193fb12a6be5c2
crizbae <crboese@gmail.com>
2022-10-27 15:39:42 -0400

    sis-scraper update
    Added multi-year functionality and dumping of professor list into json file.

--- backend/sis_scraper.py
+++ backend/sis_scraper.py
@@ -10,27 +10,35 @@ import requests
 
 
 def pog_scraper():
-    f = open('courses.json','r')
+    f = open('data/courses.json','r')
     f1 = json.load(f)
+    f.close()
     for course in f1:
         instructorStorage = []
-        year = "202209"
-        page = "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse"
-        webpage_response = requests.get(page + '?term_in=' + year + '&subj_in=' + f1[course]["subj"] + '&crse_in=' + f1[course]["ID"] + '&schd_in=L')
-        webpage = webpage_response.content
-        soup = BeautifulSoup(webpage, "html.parser")
-        times = soup.findAll("table", {
-            "class": "datadisplaytable",
-            "summary": "This table lists the scheduled meeting times and assigned instructors for this class..",
-        })
-        for time in times:
-            split_up = [x.text for x in time.findAll("td")]
-            instructor = split_up[6].split('(')[0]
-            instructor = re.sub(' +', ' ', instructor).strip()
-            if instructor != "TBA":
-                instructorStorage.append(instructor)
-        res = [*set(instructorStorage)]
-        print(res)
+        years = ["202201", "202209", "202301"]
+        for currYear in years:
+            page = "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse"
+            webpage_response = requests.get(page + '?term_in=' + currYear + '&subj_in=' + f1[course]["subj"] + '&crse_in=' + f1[course]["ID"] + '&schd_in=L')
+            webpage = webpage_response.content
+            soup = BeautifulSoup(webpage, "html.parser")
+            if soup.find(text=re.compile("No classes were found")) == None:
+                times = soup.findAll("table", {
+                    "class": "datadisplaytable",
+                    "summary": "This table lists the scheduled meeting times and assigned instructors for this class..",
+                })
+                for time in times:
+                    split_up = [x.text for x in time.findAll("td")]
+                    instructor = split_up[6].split('(')[0]
+                    instructor = re.sub(' +', ' ', instructor).strip()
+                    if instructor != "TBA":
+                        instructorStorage.append(instructor)
+        instructorStorage = [*set(instructorStorage)]
+        f1[course]['professors'] = instructorStorage
+        f2 = open('data/courses.json', 'w')
+        json.dump(f1, f2, sort_keys=True, indent=2, ensure_ascii=False)
+        f2.close()
+            
+
         
        
 
