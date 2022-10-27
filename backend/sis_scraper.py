09cab3bf6d9add130160dc6d8325c0d7c2dec33d
crizbae <crboese@gmail.com>
2022-10-27 14:33:38 -0400

    sis-scraper update
    reads in course information from course.json and finds professors. only handles fall semester at the moment.

--- backend/sis_scraper.py
+++ backend/sis_scraper.py
@@ -6,18 +6,6 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
 
-async def get_details():
-
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=5)) as session:
-        async with session.post(
-                "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse",
-                data = "term_in=202301&subj_in=MATH&crse_in=4800&schd_in=L" 
-        )as request:
-            html = await request.text()
-
-    soup = BeautifulSoup(html, 'lxml-xml')
-    return soup
-
 
 
 
@@ -25,19 +13,45 @@ def pog_scraper():
     f = open('courses.json','r')
     f1 = json.load(f)
     for course in f1:
-        year = "202301"
+        instructorStorage = []
+        year = "202209"
         page = "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse"
         webpage_response = requests.get(page + '?term_in=' + year + '&subj_in=' + f1[course]["subj"] + '&crse_in=' + f1[course]["ID"] + '&schd_in=L')
         webpage = webpage_response.content
         soup = BeautifulSoup(webpage, "html.parser")
+        times = soup.findAll("table", {
+            "class": "datadisplaytable",
+            "summary": "This table lists the scheduled meeting times and assigned instructors for this class..",
+        })
+        for time in times:
+            split_up = [x.text for x in time.findAll("td")]
+            instructor = split_up[6].split('(')[0]
+            instructor = re.sub(' +', ' ', instructor).strip()
+            if instructor != "TBA":
+                instructorStorage.append(instructor)
+        res = [*set(instructorStorage)]
+        print(res)
         
        
 
 if __name__ == "__main__":
-    year = "202209"
-    page = "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse"
-    webpage_response = requests.get(page + '?term_in=' + year + '&subj_in=' + "CSCI" + '&crse_in=' + "1100" + '&schd_in=L')
-    webpage = webpage_response.content
-    soup = BeautifulSoup(webpage, "html.parser")
-    print(soup)
+    # instructorStorage = []
+    # year = "202209"
+    # page = "https://sis.rpi.edu/rss/bwckctlg.p_disp_listcrse"
+    # webpage_response = requests.get(page + '?term_in=' + year + '&subj_in=' + "MATH" + '&crse_in=' + "1010" + '&schd_in=L')
+    # webpage = webpage_response.content
+    # soup = BeautifulSoup(webpage, "html.parser")
+    # times = soup.findAll("table", {
+    #         "class": "datadisplaytable",
+    #         "summary": "This table lists the scheduled meeting times and assigned instructors for this class..",
+    #     })
+    # for time in times:
+    #         split_up = [x.text for x in time.findAll("td")]
+    #         instructor = split_up[6].split('(')[0]
+    #         instructor = re.sub(' +', ' ', instructor).strip()
+    #         if instructor != "TBA":
+    #             instructorStorage.append(instructor)
+    # res = [*set(instructorStorage)]
+    # print(res)
+    pog_scraper()
 
