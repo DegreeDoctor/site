58bc624d1a8dc39afc104dcd05f4206818698da2
Ryzon3 <ryanmcole20@gmail.com>
2022-10-28 14:58:11 -0400

    Auto stash before merge of "CreateQuizPage" and "origin/CreateQuizPage"

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -3,102 +3,90 @@ import { useStore } from "../stores/store";
 
 export default {
     data() {
+        const majors = [
+            "Computer Science",
+            "Information Technology Web Science",
+            "Math",
+            "Physics",
+            "Chemistry",
+            "Biology",
+            "History",
+            "Geography",
+            "Economics",
+            "Psychology",
+            "Sociology",
+            "Philosophy",
+            "English",
+            "French",
+            "German",
+            "Spanish",
+            "Italian",
+            "Russian",
+            "Chinese",
+            "Japanese",
+            "Arabic",
+            "Other",
+        ];
+        const minors = [
+            "Monkey Science",
+            "Information Technology Web Science",
+            "Math",
+            "Physics",
+            "Chemistry",
+            "Biology",
+            "History",
+            "Geography",
+            "Economics",
+            "Psychology",
+            "Sociology",
+            "Philosophy",
+            "English",
+            "French",
+            "German",
+            "Spanish",
+            "Italian",
+            "Russian",
+            "Chinese",
+            "Japanese",
+            "Arabic",
+            "Other",
+        ];
         return {
             store: useStore(),
             selectedMajors: [],
             majorSearch: "",
-            minorSearch: "",
+            majorOptions: majors,
+            filteredMajors: majors,
             selectedMinors: [],
-            majorOptions: [
-                "Computer Science",
-                "Information Technology Web Science",
-                "Math",
-                "Physics",
-                "Chemistry",
-                "Biology",
-                "History",
-                "Geography",
-                "Economics",
-                "Psychology",
-                "Sociology",
-                "Philosophy",
-                "English",
-                "French",
-                "German",
-                "Spanish",
-                "Italian",
-                "Russian",
-                "Chinese",
-                "Japanese",
-                "Arabic",
-                "Other",
-            ],
-            minorOptions: [
-                "Monkey Science",
-                "Information Technology Web Science",
-                "Math",
-                "Physics",
-                "Chemistry",
-                "Biology",
-                "History",
-                "Geography",
-                "Economics",
-                "Psychology",
-                "Sociology",
-                "Philosophy",
-                "English",
-                "French",
-                "German",
-                "Spanish",
-                "Italian",
-                "Russian",
-                "Chinese",
-                "Japanese",
-                "Arabic",
-                "Other",
-            ],
+            minorSearch: "",
+            minorOptions: minors,
+            filteredMinors: minors,
         };
     },
-    computed: {
-        filterMajor() {
-            if (this.majorSearch === "") {
-                return this.majorOptions;
-            } else {
-                const needle = this.majorSearch.toLowerCase();
-                return this.majorOptions.filter(
-                    (v) => v.toLowerCase().indexOf(needle) > -1
-                );
-            }
-        },
-
-        filterMinor() {
-            if (this.minorSearch === "") {
-                return this.minorOptions;
-            } else {
-                const needle = this.minorSearch.toLowerCase();
-                return this.minorOptions.filter(
-                    (v) => v.toLowerCase().indexOf(needle) > -1
-                );
-            }
-        },
-    },
     methods: {
-        addMajor(val, done) {
+        addMajor(val) {
             if (val.length > 0) {
                 if (!this.selectedMajors.includes(val)) {
                     this.selectedMajors.push(val);
                 }
-                done(val, "toggle");
             }
         },
-
-        addMinor(val, done) {
+        addMinor(val) {
             if (val.length > 0) {
                 if (!this.selectedMinors.includes(val)) {
                     this.selectedMinors.push(val);
                 }
-                done(val, "toggle");
             }
+        },
+        filterMajor(val) {
+            this.filteredMajors = this.majorOptions.filter((x) =>
+                x.toLowerCase().includes(val.toLowerCase())
+            );
+        },
+        filterMinor(val) {
+            this.filteredMinors = this.minorOptions.filter((x) =>
+                x.toLowerCase().includes(val.toLowerCase())
+            );
         },
     },
 };
@@ -108,28 +96,29 @@ export default {
     <q-form action="https://some-url.com" method="post" class="fixed-center">
         <p>Select Your Major(s):</p>
         <q-select
-            v-model="model"
+            v-model="selectedMajors"
             filled
             use-input
             use-chips
             multiple
             input-debounce="0"
-            :options="filterMajor"
+            :options="filteredMajors"
             style="width: 250px"
             @new-value="addMajor"
+            @input-value="filterMajor"
         />
         <p>Select Your Minor(s):</p>
-
         <q-select
-            v-model="model"
+            v-model="selectedMinors"
             filled
             use-input
             use-chips
             multiple
             input-debounce="0"
-            :options="filterMinor"
+            :options="filteredMinors"
             style="width: 250px"
             @new-value="addMinor"
+            @input-value="filterMinor"
         />
     </q-form>
 </template>
