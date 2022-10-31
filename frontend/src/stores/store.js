f3ee3660258059a78337651d02e0db4c8276ead5
Ryzon3 <ryanmcole20@gmail.com>
2022-10-31 15:56:41 -0400

    Updated store.js to work with degrees

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -3,14 +3,37 @@ import { useStorage } from "@vueuse/core";
 
 export const useStore = defineStore("main", {
     state: () => ({
-        degrees: useStorage("degrees", []),
+        /* Degree is formatted as follows
+            "name" {   
+                //Name of degree user defined
+                name: "",
+                //List of major names
+                majors: [],
+                //List of minor names
+                minors: [],
+                //Name of pathway
+                pathway: "",
+                //Name of concentration
+                concentration: "",
+                // Template see program_template_specs.json in backend for it
+                template: {
+                    // Each Semester labeled by number for year and name of semester
+                    // Contains a list of courses
+                    1-Fall: [
+                    ],
+                }
+            }
+        */
+        degrees: useStorage("degrees", {}),
         // Object of lists formatted as: {"course1": 4, "Course2", 3}
         credits: useStorage("credits", {}),
+        // Current degree to view in degree view
+        selectedDegree: useStorage("selectedDegree", "")
     }),
     //Act like computed in Vue
     getters: {
-        hasDegrees: (state) => {
-            return state.degrees.length > 0;
+        getCurrentDegree: (state) => {
+            return state.degrees[state.selectedDegree];
         },
         getCredits: (state) => {
             return state.credits;
@@ -18,8 +41,18 @@ export const useStore = defineStore("main", {
     },
     //Act like methods in Vue
     actions: {
-        addDegree() {
-            this.degrees.push("Degree");
+        addDegree(degree) {
+            //this.degrees[degree.name] = degree;
+            this.degrees["Test Ryan Fun God Turbo"] = {
+                "name": "Test Ryan Fun God Turbo",
+                "majors": ["Computer Science"],
+                "minors": ["Mathematics"],
+                "pathway": "Artificial Intelligence",
+                "concentration": "Artificial Intelligence",
+            };
+        },
+        removeDegree(name) {
+            delete this.degrees[name];
         },
         addCredits(name, amount) {
             if(!this.credits[name]) {
