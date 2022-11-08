468cc4f5bcd1d8cf52d8c39a1ebec21d380a896e
liny18 <liny18@rpi.edu>
2022-11-08 17:03:26 -0500

    implemented circular progress bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <script>
 import { RouterView } from "vue-router";
 import AFooter from "./components/AFooter.vue";
+import ProgressBar from "./components/ProgressBar.vue";
 
 export default {
     components: {
         RouterView,
         AFooter,
+        ProgressBar,
     },
 };
 </script>
 
 <template>
     <q-layout view="hHh LpR fFf">
+        <ProgressBar :circle="true" />
         <q-page-container>
             <RouterView />
         </q-page-container>
---
+++ frontend/src/components/ProgressBar.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'ProgressBar',
+    props: {
+        circle: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    data () {
+        return {
+            percent: 50,
+        }
+    }
+}
+</script>
+
+<template>
+    <q-circular-progress
+        v-if = "circle"
+        show-value
+        font-size="12px"
+        :value="percent"
+        size="50px"
+        :thickness="0.22"
+        color="teal"
+        track-color="grey-3"
+        class="q-ma-md"
+    >
+    {{ percent }}%
+    </q-circular-progress>
+
+</template>
