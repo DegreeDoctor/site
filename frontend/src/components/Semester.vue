4617f04f0c8c907d9501855b600d4849f8c2fbb6
James Liu <liuxjames88@gmail.com>
2023-02-10 17:45:08 -0500

    working on making drag and drop update the store instead of being only local

--- frontend/src/components/Semester.vue
+++ frontend/src/components/Semester.vue
@@ -1,21 +1,15 @@
 <template>
     <div class="semesterContainer">
-        <h3 class="semesterName">{{ semesterName }}</h3>
-        <draggable :list="courses" group="courses" class="courseContainer" @change="log">
-            <CourseCard class="course" v-for="course in courses" :course="course" />
+        <h3 class="semesterName">{{ semester[0] }}</h3>
+        <draggable :list="courseList" group="courses" class="courseContainer" @change="log">
+            <CourseCard class="course" v-for="course in courseList" :key=course.name :course="course" />
         </draggable>
+        <q-btn push color="secondary" class="addButton" icon="add"  />
     </div>
-    <!-- <draggable :list="list" >
-      <div
-        v-for="element in list"
-        :key="element.name"
-      >
-        {{ element.name }}
-      </div>
-    </draggable> -->
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { useStore } from "../stores/store";
+import { defineComponent, toRaw } from 'vue'
 import { VueDraggableNext } from 'vue-draggable-next'
 import CourseCard from './CourseCard.vue';
 
@@ -26,19 +20,92 @@ export default defineComponent ({
     },
     data() {
         return {
-            list: [
-                { name: 'John', id: 1 },
-                { name: 'Joao', id: 2 },
-                { name: 'Jean', id: 3 },
-                { name: 'Gerard', id: 4 },
-            ],
+            store: useStore(),
             semesterName: {
                 type: String
             },
-            courses: {
-                type: Array[Object]
-            }
-
+            courseList: [
+                {
+                    "ID": "1100",
+                    "credits": [
+                        4
+                    ],
+                    "crosslisted": {},
+                    "description": "An introduction to computer programming algorithm design and analysis Additional topics include basic computer organization internal representation of scalar and array data use of topdown design and subprograms to tackle complex problems abstract data types Enrichment material as time allows Interdisciplinary case studies numerical and nonnumerical applications Students who have passed CSCI 1200 cannot register for this course",
+                    "name": "Computer Science I",
+                    "offered": {
+                        "semesters": [
+                            "fall",
+                            "spring",
+                            "summer"
+                        ],
+                        "year": "all"
+                    },
+                    "prerequisites": [],
+                    "professors": [],
+                    "properties": {
+                        "CI": false,
+                        "MR": false
+                    },
+                    "subject": "CSCI"
+                },
+                {
+                    "ID": "1010",
+                    "credits": [
+                        4
+                    ],
+                    "crosslisted": {},
+                    "description": "Functions limits continuity derivatives implicit differentiation related rates maxima and minima elementary transcendental functions introduction to definite integral with applications to area and volumes of revolution Students cannot get credit for both MATH 1010 and MATH 1500",
+                    "name": "Calculus I",
+                    "offered": {
+                        "semesters": [
+                            "fall",
+                            "spring"
+                        ],
+                        "year": "all"
+                    },
+                    "prerequisites": [],
+                    "professors": [],
+                    "properties": {
+                        "CI": false,
+                        "MR": false
+                    },
+                    "subject": "MATH"
+                },
+                {
+                    "ID": "1100",
+                    "credits": [
+                        4
+                    ],
+                    "crosslisted": {},
+                    "description": "The first semester of a twosemester sequence of interactive courses Topics include linear and angular kinematics and dynamics work and energy momentum and collisions forces and fields gravitation oscillatory motion waves sound and interference",
+                    "name": "Physics I",
+                    "offered": {
+                        "semesters": [
+                            "fall",
+                            "spring"
+                        ],
+                        "year": "all"
+                    },
+                    "prerequisites": {
+                        "one_of": [
+                            [
+                                "MATH-1010",
+                                "PHYS-1050"
+                            ]
+                        ],
+                        "required": [
+                            "PHYS-1100"
+                        ]
+                    },
+                    "professors": [],
+                    "properties": {
+                        "CI": false,
+                        "MR": false
+                    },
+                    "subject": "PHYS"
+                }
+            ]
         }
     },
     props: {
@@ -49,11 +116,34 @@ export default defineComponent ({
     },
     mounted() {
         this.semesterName = this.semester[0];
-        this.courses = JSON.parse(JSON.stringify(this.semester[1]))
+        this.courseList = []
+        for (let course of this.semester[1]) {
+            this.courseList.push(toRaw(course))
+        }
+        console.log(this.store.getCurrentDegree)
     },
     methods: {
+        debug() {
+            console.log(this.semesterName)
+            console.log(toRaw(this.courseList))
+        },
         log(event) {
+            // <!--! this is not reactive right now, still have to change that
+                // <!--! it does update the store (i think), but it doesn't update the view
+                    // <!--! need to look at the docs for pinia more to fix this
             console.log(event)
+            if (event["added"]) {
+                this.store.updateDegreeSemester(this.semesterName, event.added.element, "add")
+            }
+            // this.store.updateDegreeSemester(this.semesterName, toRaw(this.courseList))
+            // console.log(this.store.getCurrentDegree)
+        },
+        addCoursetoStore(event) {
+            console.log(event)
+            // this.store.updateDegreeSemester(this.semesterName, this.courseList, "add")
+        },
+        removeCourseFromStore(event) {
+
         }
     }
 })
@@ -73,6 +163,8 @@ export default defineComponent ({
         border: 1px solid black;
         background-color: #D9D9D9;
         min-width: 100%;
+        display: flex;
+        flex-direction: column;
     }
 
     .courseContainer {
@@ -90,5 +182,9 @@ export default defineComponent ({
         padding: 5px;
         border-radius: 5px;
         color: white;
+    }
+
+    .addButton {
+        margin: auto 5px 5px auto;
     }
 </style>
--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -90,6 +90,19 @@ export const useStore = defineStore("main", {
         toggleDarkMode() {
             this.darkMode = !this.darkMode;
         },
+        updateDegreeSemester(semester, course, action) {
+            if (action == "add") {
+                console.log(`adding to ${semester}`)
+                this.degrees[this.selectedDegree].template[semester].push(course);
+                console.log(this.degrees[this.selectedDegree].template[semester])
+            } else if (action == "remove") {
+                this.degrees[this.selectedDegree].template[semester] = 
+                    this.degrees[this.selectedDegree].template[semester].filter(
+                        (item) => item.name != course.name
+                    );
+            }
+
+        },
         deleteEverything() {
             Object.keys(this.degrees).forEach( degreeName => {
                 delete this.degrees[degreeName]
