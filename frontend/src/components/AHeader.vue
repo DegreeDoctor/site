6a63f016a78b5eefbcba1eb5654078eb4b416259
William Chen <williamchen259@gmail.com>
2023-02-28 17:27:02 -0500

    Icon Animation Part 1

--- frontend/src/components/AHeader.vue
+++ frontend/src/components/AHeader.vue
@@ -1,13 +1,15 @@
 <script>
+import { useImage } from "@vueuse/core";
 import { useStore } from "../stores/store";
 import DarkModeToggle from "./DarkModeToggle.vue";
 import ProgressBar from "./ProgressBar.vue";
 
 export default {
     components: {
-        DarkModeToggle,
-        ProgressBar,
-    },
+    DarkModeToggle,
+    ProgressBar,
+    useImage
+},
     data() {
         return {
             current: useStore().getCurrentDegreeName,
@@ -40,10 +42,10 @@ export default {
 };
 </script>
 
-<template>
+<!-- <template>
     <q-header elevated class="bg-secondary text-white" height-hint="98">
         <q-toolbar>
-            <q-toolbar-title>
+            <q-toolbar-title to="/">
                 <q-avatar>
                     <img src="../assets/Degree_Doctor_logo.png" />
                 </q-avatar>
@@ -116,14 +118,179 @@ export default {
             </div>
         </q-toolbar>
     </q-header>
+</template> -->
+<template>
+    <nav> 
+        <div class="navbar">
+            <div class="image-container">
+                <router-link to="/quiz" id="logo"><img src="../assets/Degree_Doctor_logo.png"/></router-link>
+            </div>
+            <div class="DD">
+                <RouterLink to="/" id="title">Degree Doctor</RouterLink>
+                <div class="fading-effect"></div>
+            </div>
+            <div class="other">
+                <DarkModeToggle/>
+                <RouterLink to="/quiz" id="plans">My Plans</RouterLink>
+            </div>
+        </div>
+    </nav>  
 </template>
 
 <style scoped>
+.navbar{
+    display: flex;
+    background:turquoise;
+    justify-content: space-around;
+    align-items: center;
+    height: 5rem;
+}
+.image-container {
+  position: absolute;
+  top: 5%;
+  left: 25%;
+  transform: translate(-50%,-50%);
+  z-index: 2; /* Makes sure this is on top */
+}
+.image-container img {
+  -webkit-filter: drop-shadow(-4px 5px 5px rgba(0,0,0,0.6));
+  filter: drop-shadow(-4px 5px 5px rgba(0,0,0,0.6));
+  height: 200px;
+  animation: image-slide 4s cubic-bezier(.5,.5,0,1);
+  animation-fill-mode: forwards;
+}
+.DD {
+  position: absolute;
+  top: 5%;
+  transform: translate(-50%,-50%);
+  z-index: 1; /* Places this below the image container */
+  margin-left: -700px;
+}
+.DD .fading-effect {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  background: turquoise;
+  -webkit-animation: text-slide 4s cubic-bezier(1, 0, 0.5, 0.5);
+  animation: text-slide 4s cubic-bezier(1, 0, 0.5, 0.5);
+  animation-fill-mode: backwards;
+  -webkit-animation-fill-mode: backwards;
+}
+.fading-effect {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  background: white;
+}   
+/* Slides the image from left (-250px) to right (150px) */
+@keyframes image-slide {
+  0% { transform: translateX(150px) scale(0); }
+  60% { transform: translateX(150px) scale(1); }
+  90% { transform: translateX(-250px) scale(1); }
+  100% { transform: translateX(-250px) scale(1); }  
+}
+
+/* Slides the text by shrinking the width of the object from full (100%) to nada (0%) */
+@keyframes text-slide {
+    100% { width: 100%; }
+    75%{ width: 100%; }
+    60% { width: 0; }
+    0% { width: 0; }
+  /* 0% { width: 100%; }
+  60% { width: 100%; }
+  75%{ width: 0; }
+  100% { width: 0; } */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#title{
+    display:flex;
+    color:black;
+    text-decoration: none;
+    font-size: 1.2rem;
+    padding-bottom: 3px;
+    justify-content: space-around;
+    /* position: relative; */
+
+}
+/* #logo{
+    animation: 1s slideright;
+} */
+/* @keyframes slideleft{
+    from {
+    margin-left: -10%;
+    width: 300%;
+  }
+
+  to {
+    margin-right: 0%;
+    width: 500%;
+  }
+} */
+@keyframes slideright{
+    from {
+    margin-left: -10%;
+    width: 200%;
+  }
+
+  to {
+    margin-right: 0%;
+    width: 300%;
+  }
+}
+
+
+#plans{
+    /* color:white;  */
+    text-decoration:none;
+}
+.navbar-left{
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+    animation: 1s slideright ;
+    animation-fill-mode: forwards;
+}
+
+.navbar-right{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center; 
+
+}
+
+.navbar img{
+    width: 3rem;
+    height: 3rem;
+}
+button{
+    background-color: blue;
+    border-radius: .2rem;
+}
 .card-contain {
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     padding-right: 1em;
+}
+.Degree_Doctor{
+    font: 2rem;
+    text-decoration: none;
 }
 </style>
