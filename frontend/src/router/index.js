ba73610aac8211e5d7e50e24401f9a13f39eaabb
Ryzon3 <ryanmcole20@gmail.com>
2023-04-11 17:13:52 -0400

    Fixed some things for deploy

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useStore } from "../stores/store";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    mode: "hash",
+    history: createWebHashHistory(),
     routes: [
         {
             path: "/",
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/pages/",
+    base: "/",
     plugins: [
         vue({
             template: { transformAssetUrls },
